25f501c615fc5361f909367b0fc9bd9042933059
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2021-03-09 10:50:43 +0200

    ShiftValidator Initial

---
+++ ShiftValidator/ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HelperLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var shift = new Shift("12:00-16:00");
+            Console.WriteLine(shift.ClockedShift);
+            System.Threading.Thread.Sleep(2500);
+        }
+    }
+}
---
+++ ShiftValidator/HelperLibrary/Shift.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperLibrary
+{
+    public class Shift
+    {
+        private string _clockedShift;
+        private DateTime _dateTime { get; set; }
+
+        public string ClockedShift
+        {
+            get => getTime(_clockedShift);
+            // Is the given value valid?
+            set => _clockedShift = validate(value) ? value : null;
+        }
+
+
+
+        public Shift(string aClockedShift, DateTime aDateTime)
+        {
+            ClockedShift = aClockedShift;
+            _dateTime = aDateTime;
+        }
+
+        // validate the formality of the given shift
+        private bool validate(string value)
+        {
+            return true;
+        }
+
+
+        // Get the shift in decimals
+        private string getTime(string _clockedShift)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
